c8e3ac053d8dee84a026d200ba9c6ef10c2c4911
Trace Wynecoop <trwyne03@gmail.com>
2023-02-09 10:17:28 -0800

    restructure into react app

--- old-frontend-files/Congregation.js
+++ old-frontend-files/Congregation.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
 'use strict';
+
 const e = React.createElement;
 
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
+
+/*
 function Congregation() {
     return (
         <div class="congregation">
@@ -18,7 +43,4 @@ function Congregation() {
         </div>
     );
 }
-
-const domContainer = document.querySelector('#congregation');
-const root = ReactDOM.createRoot(domContainer);
-root.render(e(Congregation));
+*/

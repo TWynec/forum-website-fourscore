60421400d9ed21f52f6173a697416e177de11913
Trace Wynecoop <trwyne03@gmail.com>
2023-02-17 12:44:42 -0800

    add posting and sign-in mockup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './styles/App.css';
+import React, { useState } from 'react';
+
+function Post({ post }) {
+  return (
+    <div>
+      <b>{post.username}</b>
+      <p>{post.text}</p>
+    </div>
+  );
+}
+
+function PostInput({ profile, onSubmit }) {
+  const [postText, setPostText] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit({ username: profile.name, text: postText });
+    setPostText('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <textarea value={postText} onChange={(event) => setPostText(event.target.value)} />
+      <button type="submit">Post</button>
+    </form>
+  );
+}
+
+function ProfileForm({ profile, onProfileSubmit }) {
+  const [username, setUsername] = useState(profile.name);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onProfileSubmit({ name: username });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+    </form>
+  );
+}
+
+function SignInForm({ onSignIn }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSignIn({ username, password });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input value={username} onChange={(event) => setUsername(event.target.value)} />
+      <input value={password} onChange={(event) => setPassword(event.target.value)} type="password" />
+      <button type="submit">Sign In</button>
+    </form>
+  );
+}
 
 function App() {
+  const [signedIn, setSignedIn] = useState(false);
+  const [profile, setProfile] = useState({ name: '' });
+  const [posts, setPosts] = useState([]);
+
+  const handleSignIn = (credentials) => {
+    setProfile({ name: credentials.username });
+    setSignedIn(true);
+  };
+
+  const handleProfileSubmit = (profile) => {
+    setProfile(profile);
+  };
+
+  const handleSubmit = (post) => {
+    setPosts([post, ...posts]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {signedIn ? (
+        <>
+          <ProfileForm profile={profile} onProfileSubmit={handleProfileSubmit} />
+          <PostInput profile={profile} onSubmit={handleSubmit} />
+          {posts.map((post) => (
+            <Post key={post.text} post={post} />
+          ))}
+        </>
+      ) : (
+        <SignInForm onSignIn={handleSignIn} />
+      )}
     </div>
   );
 }

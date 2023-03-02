11b296cea2212a637b2edab76083e8d5958f72e4
Anna-Mclenko <amikhailenko@ewu.edu>
2023-03-02 11:10:06 -0800

    start on the css. working on post react css

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import img from "../styles/2955-removebg-preview.png"
 //import "../styles/App.css";
 import "../styles/navbar.css";
 
 function navbar() {
   return (
      <div className="App-header">
-
+         <img className="App-logo" src={img} alt="Website Logo"/>
      <nav>
       <ul>
         <li>
